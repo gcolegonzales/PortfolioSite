16071a683e9b7814005bae558a9df691d272f7cc
Cole Gonzales <colegonzales@Coles-MBP.attlocal.net>
2020-02-14 16:50:15 -0600

    Added to tech line

--- src/components/tech-line.js
+++ src/components/tech-line.js
@@ -6,18 +6,26 @@ import Sql from '../images/sql1.png'
 import ExpressImg from '../images/express.png'
 import KnexImg from '../images/knex.png'
 import ReduxImg from '../images/redux.png'
+import SharepointImg from '../images/sharepoint.jpeg'
+import CSSImg from '../images/css.png'
+import HTMLImg from '../images/html.png'
+import PostGresImg from '../images/postgres.png'
 import styled from 'styled-components'
 
 function TechLine() {
     return (
         <TileWrapper>
             <JSTile src={JS} alt="Javascript" />
-            <Tile src={Node} alt="Javascript" />
-            <Tile src={ReactImg} alt="Javascript" />
-            <Tile src={ReduxImg} alt="Javascript" />
-            <Tile src={ExpressImg} alt="Javascript" />
-            <Tile src={KnexImg} alt="Javascript" />
-            <Tile src={Sql} alt="Javascript" />
+            <Tile src={Node} alt="Node.js" />
+            <Tile src={ReactImg} alt="React" />
+            <Tile src={ReduxImg} alt="Redux" />
+            <Tile src={ExpressImg} alt="Express" />
+            <Tile src={Sql} alt="Sqlite3" />
+            <Tile src={KnexImg} alt="Knex.js" />
+            <Tile src={CSSImg} alt="CSS" />
+            <Tile src={HTMLImg} alt="HTML5" />
+            <Tile src={PostGresImg} alt="PostgreSQL" />
+            <Tile src={SharepointImg} alt="Sharepoint" />
         </TileWrapper>
     )
 }
